918d027e79ef3cb2f91ce7fdfaf64bc6305047d8
johnny <>
2020-09-30 10:04:48 +0800

    FIX quick message size

--- lib/pages/live_chat_room/chat_room_feature/center_area/chat_area/quick_message/quick_message.dart
+++ lib/pages/live_chat_room/chat_room_feature/center_area/chat_area/quick_message/quick_message.dart
@@ -12,11 +12,12 @@ class QuickMessage extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
-          MessageItem(text: '初次见面',),
+          MessageItem(text: '初次见面 😀',),
           MessageItem(text: '早安',),
-          MessageItem(text: '主播今天特别帅',),
+          MessageItem(text: '主播今天特别帅 🥰',),
           MessageItem(text: '主播今天特别美',),
           MessageItem(text: '好久不见',),
+          MessageItem(text: '我來幫你加油! ❤️',),
         ],
       ),
     );
@@ -32,18 +33,23 @@ class MessageItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      margin: EdgeInsets.only(right: 10.0),
-      padding: EdgeInsets.symmetric(vertical: 1.0,horizontal: 8.0),
-      decoration: BoxDecoration(
-        color: Colors.black38,
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      child: Text(text,style: TextStyle(
-        fontSize: 10.0,
-        color: Colors.white,
-      ),),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(right: 4.0),
+          padding: EdgeInsets.symmetric(vertical: 3.0,horizontal: 8.0),
+          decoration: BoxDecoration(
+            color: Colors.black38,
+            borderRadius: BorderRadius.circular(6.0)
+          ),
+          child: Text(text,style: TextStyle(
+            fontSize: 10.0,
+            color: Colors.white,
+          ),),
+        ),
+      ],
     );
   }
 }
